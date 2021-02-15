209b231f62f2204ad8855eb6c797ad80bc278933
Renar Suurpere <renar.suurpere@khk.ee>
2021-02-15 19:24:26 +0200

    Loodud suunamise abifunktsioon

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -52,7 +52,7 @@ class users extends Controller
                 $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
                 if ($this->usersModel->register($data)) {
                     message('register_success', 'You are registred and now can log in');
-                    header('Location: ' . URLROOT . '/' . 'users/login');
+                    redirect('users/login');
                 } else {
                     die('Something went wrong');
                 }
@@ -97,7 +97,7 @@ class users extends Controller
                 $loggedInUser = $this->usersModel->login($data['email'], $data['password']);
                 if ($loggedInUser) {
                     $this->createUserSession($loggedInUser);
-                    header('Location: ' . URLROOT . '/' . 'pages/index');
+                    redirect('pages/index');
                 } else {
                     $data['password_err'] = 'Passwird us incorrect';
                     $this->view('users/login', $data);
@@ -118,7 +118,7 @@ class users extends Controller
     {
         session_unset();
         session_destroy();
-        header('Location: ' . URLROOT . '/' . 'users/login');
+        redirect('users/login');
     }
 
     public function createUserSession($user)
