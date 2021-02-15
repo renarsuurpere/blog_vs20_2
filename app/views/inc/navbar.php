82b7c87934cf8c5c789ebb1e46d7c74529a6c993
Renar Suurpere <renar.suurpere@khk.ee>
2021-02-15 14:37:29 +0200

    Lisatud rakenduse navigatsiooni põhipaneel

---
+++ app/views/inc/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+    <div class="container">
+        <a href="<?php echo URLROOT; ?>" class="navbar-brand"><?php echo PROJECT_NAME ?></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT; ?>" class="nav-link">Home</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
