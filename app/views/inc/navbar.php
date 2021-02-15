a48c1bfba295b8c10f47627172af68e0ee81acac
Renar Suurpere <renar.suurpere@khk.ee>
2021-02-15 19:16:39 +0200

    Loodud sisse logitud kasutaja navigatsiooni riba

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -12,12 +12,18 @@
                 </li>
             </ul>
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a href="<?php echo URLROOT; ?>/users/register" class="nav-link">Register</a>
-                </li>
-                <li class="nav-item">
-                    <a href="<?php echo URLROOT; ?>/users/login" class="nav-link">Login</a>
-                </li>
+                <?php if (isset($_SESSION['user_id'])): ?>
+                    <li class="nav-item">
+                        <a href="<?php echo URLROOT; ?>/users/logout" class="nav-link">Logout</a>
+                    </li>
+                <?php else: ?>
+                    <li class="nav-item">
+                        <a href="<?php echo URLROOT; ?>/users/register" class="nav-link">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?php echo URLROOT; ?>/users/login" class="nav-link">Login</a>
+                    </li>
+                <?php endif; ?>
             </ul>
         </div>
     </div>
