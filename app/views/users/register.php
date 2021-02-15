5b90c87c6746d7d57686426cded0b6ca75d4ef21
Renar Suurpere <renar.suurpere@khk.ee>
2021-02-15 15:32:22 +0200

    Loodud kasutajate registreerimisvorm

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -1,3 +1,38 @@
 <?php require_once APPROOT . '/views/inc/header.php'; ?>
-<h1>Add user</h1>
+<div class="row">
+    <div class="col-md-6 mx-auto">
+        <!-- form register -->
+        <div class="card card-outline-secondary mt-5">
+            <div class="card-header">
+                <h3 class="mb-0">Sign up</h3>
+                <p class="mt-2">Please fill the fields below to register</p>
+            </div>
+            <div class="card-body">
+                <form role="form" class="form">
+                    <div class="form group">
+                        <label for="name">Name<sup>*</sup></label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Full name">
+                    </div>
+                    <div class="form group">
+                        <label for="email">Email<sup>*</sup></label>
+                        <input type="text" class="form-control" id="email" name="email" placeholder="Email">
+                    </div>
+                    <div class="form group">
+                        <label for="password">Password<sup>*</sup></label>
+                        <input type="password" class="form-control" id="password" name="password"
+                               placeholder="Password">
+                    </div>
+                    <div class="form group">
+                        <label for="confirm_password">Password<sup>*</sup></label>
+                        <input type="password" class="form-control" id="confirm_password" name="confirm_password"
+                               placeholder="Confirm password">
+                    </div>
+                    <div class="form group">
+                        <button class="btn btn-success btn-lg float-right" type="submit">Register</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 <?php require_once APPROOT . '/views/inc/footer.php'; ?>
