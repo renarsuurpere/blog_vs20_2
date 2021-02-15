9c617f2511211ca1edaddf93961074fedf12b501
Renar Suurpere <renar.suurpere@khk.ee>
2021-02-15 16:41:36 +0200

    Loodud parooli ja emaili väärtuste valideerimine

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -9,13 +9,46 @@ class users extends Controller
      */
     public function __construct()
     {
-        $usersModel = $this->model('User');
+        $this->usersModel = $this->model('User');
     }
 
     public function register()
     {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            print_r($_POST);
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $data = array(
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'confirm_password' => trim($_POST['confirm_password']),
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password_err' => ''
+            );
+            if (empty($data['name'])) {
+                $data['name_err'] = 'Please enter the name';
+            }
+            if (empty($data['email'])) {
+                $data['email_err'] = 'Please enter the email';
+            } else if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+                $data['email_err'] = 'Please enter the valid email';
+            } else if ($this->usersModel->findUserByEmail($data['email'])) {
+                $data['email_err'] = 'Email is already taken';
+            }
+            if (empty($data['password'])) {
+                $data['password_err'] = 'Please enter the password';
+            } else if (strlen($data['password']) < 6) {
+                $data['password_err'] = 'Password must consist at least 6 characters';
+            }
+            if (empty($data['confirm_password'])) {
+                $data['confirm_password_err'] = 'Please enter the confirm password';
+            } else if (strlen($data['confirm_password']) < 6) {
+                $data['confirm_password_err'] = 'Password must consist at least 6 characters';
+            } else if ($data['password'] !== $data['confirm_password']) {
+                $data['confirm_password_err'] = 'Passwords do not match';
+            }
+            print_r($data);
         } else {
             $this->view('users/register');
         }
--- app/models/User.php
+++ app/models/User.php
@@ -3,5 +3,26 @@
 
 class User
 {
+    private $db;
+
+    /**
+     * User constructor.
+     */
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function findUserByEmail($email)
+    {
+        $this->db->query('SELECT * FROM users WHERE email=:email');
+        $this->db->bind(':email', $email);
+        $user = $this->db->getOne();
+        if ($this->db->rowCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
