b4fc3dcd9a57af797dcd0d8ff284328f60413bb3
Renar Suurpere <renar.suurpere@khk.ee>
2021-03-11 12:18:31 +0200

    Loodud siltide nimekirja kuvamine blogi lehel

---
+++ app/controllers/tags.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class tags extends Controller
+{
+    public function __construct()
+    {
+        $this->tagsModel = $this->model('Tag');
+    }
+
+    public function index()
+    {
+        $tags = $this->tagsModel->getTags();
+        $data = array('tags' => $tags);
+        $this->view('tags/index', $data);
+    }
+}
